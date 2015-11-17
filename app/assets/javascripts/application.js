1a2cb97bfc6becffa74a0a3732451b7aebf46493
Aviv Levinsky <pugsiman@gmail.com>
2015-11-17 08:27:57 +0200

    Remove deflater, unstable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//require jquery_ujs
+// require jquery_ujs
 //= require turbolinks
 //= require_tree .
