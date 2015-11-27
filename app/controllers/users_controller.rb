2aab5579bf82dc658343a5bac1cbadcd02f645ef
Aviv Levinsky <pugsiman@gmail.com>
2015-11-27 02:41:06 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Hard to believe but it actually worked."
+      redirect_to @user
+    else
+      flash.now[:danger] = "Error, bitch."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+    end
 end
