abd08e04a52dca8a2672c52bf8be2ccc457bc572
Aviv Levinsky <pugsiman@gmail.com>
2015-12-09 08:25:52 +0200

    Only show activated users and better performance for activate and create_reset_digest methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
-  before_save :downcase_email
+  before_save   :downcase_email
   before_create :create_activation_digest
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name,     presence: true, length: { maximum: 35 }
@@ -36,8 +36,7 @@ class User < ActiveRecord::Base
   end
 
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   def send_activation_email
@@ -46,8 +45,8 @@ class User < ActiveRecord::Base
 
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
   end
 
   def send_password_reset_email
